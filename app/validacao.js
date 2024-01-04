03639319e4c0275a51fe69931309fb80d3d9ab74
Leonardo Ivson <leonardoivson18@gmail.com>
2024-01-04 17:44:09 -0400

    publicando jogo dos números secretos.

---
+++ app/validacao.js
@@ -0,0 +1,44 @@
+function verificaSeOChutePossuiValorValido(){
+    const numero = +chute 
+    if (chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor Invalido</div>'
+        return
+    }
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML +=`
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return 
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id= "jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-angle-down"></i></div> 
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-angle-up"></i></div> 
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+
+document.body.addEventListener('click',e => {
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
